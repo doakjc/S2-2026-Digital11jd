c3e1161567c6c2b597e0e5c70eaf046b2ca8fb1c
doakjc <165220132+doakjc@users.noreply.github.com>
2026-07-21 03:34:43 +0000

    21/7 - Initial commit with phrase clean

--- starter.py
+++ starter.py
@@ -2,3 +2,10 @@
 raw_phrase = "aP!pL3e#S4aU%cE"
 
 # YOUR GOAL: Clean up this phrase using the 3 Security Rules below!
+clean_phrase = ""
+
+for char in raw_phrase:
+    if char.isalpha():
+        clean_phrase += char.upper()
+
+print(clean_phrase)
